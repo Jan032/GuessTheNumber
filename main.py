aca63715278fbb6e1b17bb12e66da18c4e5766e7
Jan Krajnc <jan.krajnc17@gmail.com>
2021-10-10 18:03:09 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+secret = 4
+guess = int(input("ugotovi številko ( ͡ಥ ͜ʖ ͡ಥ) "))
+if guess == secret:
+    print("WOHOO YOU ARE THE WINNER OF ABSOLUTELY NOTHING!!!!!!! (✿❦ ͜ʖ ❦)")
+elif guess == guess > 10:
+    print("HAHAHA! you are far off-.-")
+elif guess == guess > 4:
+    print("a bit lower might do the trick")
+elif guess == guess < 4:
+    print("gotta have to go a little higher than that, run the program again")
